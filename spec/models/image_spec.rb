1b8ad66939924c07257b6633d67bebc03641607d
ryutaro-hirasawa <doraguna0219@gmail.com>
2020-11-12 15:02:08 +0900

    test post

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Image do
+  describe '#create'do
+  it 'note_id（外部キー）が存在すれば投稿可' do
+    image = build(:image)
+    expect(image).to be_valid
+  end
+  end
 end
