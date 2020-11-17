e4c05c56539c07cf76e7dc03a41877f511189a9e
ryutaro-hirasawa <doraguna0219@gmail.com>
2020-11-17 18:40:41 +0900

    setting note_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "notes#index"
-  resources :notes, only: [:index, :new, :create, :show, :destroy] do
+  resources :notes do
     resources :comments, only: :create
     resource :favorites, only: [:create, :destroy]
   end
