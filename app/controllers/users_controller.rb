8de037edcd06e9c718d185584c06b96042893139
ryutaro-hirasawa <doraguna0219@gmail.com>
2020-11-18 15:36:41 +0900

    setting delete_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,12 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :birthday ,presence: true
-  has_many :notes
-  has_many :comments
+  has_many :notes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 end
